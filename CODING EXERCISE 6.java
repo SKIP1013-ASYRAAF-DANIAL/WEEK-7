3a006ec0b29cca23cb7851684f6257e686d5ffac
AHMAD ASYRAAF DANIAL BIN NORAZLAN 284368 <92504346+AsyraafDanial-284368@users.noreply.github.com>
2021-12-02 01:18:53 +0800

    Update CODING EXERCISE 6.java

--- CODING EXERCISE 6.java
+++ CODING EXERCISE 6.java
@@ -2,7 +2,7 @@ package exercise;
 
 import java.util.Scanner;
 
-public class Exercise6Tan {
+public class Exercise6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
